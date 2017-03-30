620ccedd4b69b643a48cc24bcd6359bb11cb81f5
Juan Resendiz <jresendiz27@gmail.com>
2017-03-29 23:42:17 -0600

    Adding new function to sanitize strings
    Fixing issue with id and title

--- grails-app/controllers/mx/ipn/siipg/reports/ReportsController.groovy
+++ grails-app/controllers/mx/ipn/siipg/reports/ReportsController.groovy
@@ -7,14 +7,14 @@ class ReportsController {
     def chartService
 
     def dummyReport() {
-        Content content = Content.findByTitleLike("%${params.name}%")
+        Content content = Content.findById(params.int("id"))
         response.setContentType("application/pdf")
         response.setHeader("Content-Disposition", "attachment; filename=${StringUtil.sanitize(content.title)}.pdf")
         renderPdf(template: "main", model: [content: content, image: chartService.generateBase64ImageURI(chartService.generateDummyChart())])
     }
 
     def generateReport() {
-        Content content = Content.findByTitleLike("%${params.name}%") ?: Content.findById(params.int("id"));
+        Content content = Content.findByTitleLike("%${StringUtil.sanitizeTitle(params.name)}%") ?: Content.findById(params.int("id"));
         response.setContentType("application/pdf")
         response.setHeader("Content-Disposition", "attachment; filename=${StringUtil.sanitize(content.title)}.pdf")
         renderPdf(template: "main", model: [content: content, image: params.image ?: chartService.generateBase64ImageURI(chartService.genereteBarChart())])
--- src/main/groovy/mx/ipn/siipg/reports/util/StringUtil.groovy
+++ src/main/groovy/mx/ipn/siipg/reports/util/StringUtil.groovy
@@ -11,4 +11,11 @@ class StringUtil {
         sanitizedString = sanitizedString.replaceAll("[\\\\|/|:|\\*|\\?|\\<|\\>|\\|]", "_")
         return sanitizedString
     }
+
+    static String sanitizeTitle(String  string) {
+        String sanitizedString = string
+        sanitizedString = sanitizedString.trim()
+        sanitizedString = sanitizedString.replaceAll(".", "%")
+        return sanitizedString
+    }
 }
