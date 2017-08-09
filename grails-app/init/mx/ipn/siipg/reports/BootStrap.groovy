54ef58f1f25283b30e430cfa99e9b6acc73e79b3
Juan Resendiz <jresendiz27@gmail.com>
2017-08-08 19:36:38 -0500

    Adding new configurations for developmetn

--- grails-app/init/mx/ipn/siipg/reports/BootStrap.groovy
+++ grails-app/init/mx/ipn/siipg/reports/BootStrap.groovy
@@ -8,10 +8,17 @@ class BootStrap {
     def dataSource
 
     def init = { servletContext ->
-        if(Environment.current == Environment.PRODUCTION) {
+        try {
+            Content.list().each {
+                it.delete(flush: true)
+            }
+        } catch (Exception e) {
+            e.printStackTrace()
+        }
+        if (Environment.current == Environment.PRODUCTION) {
             def resource = this.class.classLoader.getResourceAsStream('public/inserts.sql')
             def file = resource.text;
-            Sql connection =  new Sql(dataSource)
+            Sql connection = new Sql(dataSource)
             connection.execute(file);
         }
     }
